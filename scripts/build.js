d7ebd63fae97a4e0c42fc7a913af537d1dbfcc52
dsrkafuu <dsrkafuu@outlook.com>
2021-02-28 18:05:42 +0800

    fix: iife error bundle issue

--- scripts/build.js
+++ scripts/build.js
@@ -1,15 +1,15 @@
-const path = require("path");
-const fs = require("fs");
-const esbuild = require("esbuild");
-const babel = require("@babel/core");
-const terser = require("terser").minify;
+const path = require('path');
+const fs = require('fs');
+const esbuild = require('esbuild');
+const babel = require('@babel/core');
+const terser = require('terser').minify;
 
 /* esm file */
 esbuild.buildSync({
-  entryPoints: ["src/index.esm.js"],
+  entryPoints: ['src/index.esm.js'],
   outfile: "lib/index.esm.js",
-  format: "esm",
-  target: "es6",
+  format: 'esm',
+  target: 'es6',
   bundle: true,
   minify: true,
   sourcemap: true,
@@ -19,13 +19,14 @@ esbuild.buildSync({
 
 // build
 esbuild.buildSync({
-  entryPoints: ["src/index.js"],
-  outfile: "lib/vector.min.js",
-  format: "iife",
-  target: "es2020",
+  entryPoints: ['src/index.js'],
+  outfile: 'lib/vector.min.js',
+  format: 'iife',
+  target: 'es2020',
+  bundle: true,
 });
-const output = path.resolve(__dirname, "../lib/vector.min.js");
-let content = fs.readFileSync(output, { encoding: "utf-8" });
+const output = path.resolve(__dirname, '../lib/vector.min.js');
+let content = fs.readFileSync(output, { encoding: 'utf-8' });
 
 // transform
 content = babel.transformSync(content).code;
